b71db3e3f6e07e3a590c6ed1d43a7d4bee351517
Orion <contact@oriondev.fr>
2026-01-29 23:30:26 +0100

    feat: github calendar

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { ActivityCalendar } from "react-activity-calendar";
+
+export type Activity = { date: string; count: number; level: 0 | 1 | 2 | 3 | 4; };
+
+export function GithubCalendar() {
+    const [data, setData] = useState<Activity[] | null>(null);
+
+    useEffect(() => {
+        fetch(`https://github-contributions-api.jogruber.de/v4/oriionn?y=${new Date().getFullYear()}`)
+            .then((res) => res.json())
+            .then((json) => setData(json.contributions));
+    }, []);
+
+    if (!data) return null;
+
+    return (
+        <ActivityCalendar
+            data={data}
+            maxLevel={4}
+            colorScheme="light"
+            className="calendar"
+        />
+    );
+}
