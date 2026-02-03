f4fc3dad3e88391c87e4ea1ffac638783be945a4
Orion <contact@oriondev.fr>
2026-02-03 18:41:33 +0100

    refactor: translation utils

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -1,5 +1,20 @@
 import { getCollection } from "astro:content";
 import type { Translation } from "../content.config";
+import type { AstroGlobal } from "astro";
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+
+export async function getLocale(
+    Astro: Readonly<AstroGlobal<Record<string, any>, AstroComponentFactory, Record<string, string | undefined>>>
+): Promise<{ translation: Translation, lang: string }> {
+    const translations = await getCollection("translations");
+    let lang = Astro.params.lang ?? "en";
+
+    let translation = translations.filter((translation: { id: string }) => translation.id === lang);
+    return {
+        translation: translation[0].data,
+        lang
+    }
+}
 
 export async function getTranslation(lang: string): Promise<Translation> {
     const translations = await getCollection("translations");
